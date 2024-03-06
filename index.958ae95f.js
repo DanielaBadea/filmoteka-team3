d9a33419e948448ac773d967fa93430badee371a
DanielaBadea <142940948+DanielaBadea@users.noreply.github.com>
2024-03-06 18:24:41 +0000

    Deploying to gh-pages from @ DanielaBadea/filmoteka-team3@fee6ea5fffd2409f2bfeaa8fc6055df3116c30e9 🚀

---
+++ index.958ae95f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".toggle-theme"),t=document.querySelector("body");e.addEventListener("click",(function(){t.classList.toggle("dark-mode"),t.classList.toggle("light-mode")}))}));
+//# sourceMappingURL=index.958ae95f.js.map
