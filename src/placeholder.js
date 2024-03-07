725de8a26a001e33a5bde546314e693cf7836833
DanielaBadea <badeadaniella@gmail.com>
2024-03-07 16:46:25 +0200

    header

---
+++ src/placeholder.js
@@ -0,0 +1,12 @@
+function removePlaceholder() {
+   document.querySelector("#header-input").removeAttribute("placeholder");
+
+}
+
+function restorePlaceholder(){
+    const placeholder = document.querySelector("#header-input");
+    if( placeholder.value === ""){
+        placeholder.setAttribute("placeholder", "Movie search")
+    }
+}
+
