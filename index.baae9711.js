95390abeee33b414573112ed2b570fd90c6acc29
DanielaBadea <142940948+DanielaBadea@users.noreply.github.com>
2024-03-07 14:48:53 +0000

    Deploying to gh-pages from @ DanielaBadea/filmoteka-team3@725de8a26a001e33a5bde546314e693cf7836833 🚀

---
+++ index.baae9711.js
@@ -0,0 +1,2 @@
+function removePlaceholder(){document.querySelector("#header-input").removeAttribute("placeholder")}function restorePlaceholder(){var e=document.querySelector("#header-input");""===e.value&&e.setAttribute("placeholder","Movie search")}
+//# sourceMappingURL=index.baae9711.js.map
