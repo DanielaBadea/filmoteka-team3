26a33718d2b5d9b68eacd8af9991e07cce23bbfd
DanielaBadea <badeadaniella@gmail.com>
2024-03-09 12:04:36 +0200

    body-color-on-dark

--- src/js/theme-mode.js
+++ src/js/theme-mode.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const themeToggleBtn = document.querySelector('.toggle-theme');
     const sunIcon = document.querySelector('.sun');
     const moonIcon = document.querySelector('.moon');
+    const spanText = document.querySelector('.span-text')
+    const btnStudents = document.querySelector('.students')
+    const footerBtn = document.querySelector('.footer-container-btn')
     sunIcon.style.fill = '#f5f84d';
     themeToggleBtn.addEventListener('click', () => {
         document.body.classList.toggle('light-mode');
@@ -10,11 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
             sunIcon.style.display = 'block';
             moonIcon.style.display = 'none';
             document.body.style.backgroundColor = "#fff"; 
+            spanText.style.color ="#545454";
+            btnStudents.style.color="#545454";
+            footerBtn.style.color ="#545454";
             sunIcon.style.fill = '#f5f84d';
         } else { // Dacă modul dark este activat
             moonIcon.style.display = 'block';
             sunIcon.style.display = 'none';
             document.body.style.backgroundColor = "#000000e6"; 
+            document.body.style.color = "#fff";
+            spanText.style.color ="#fff";
+            btnStudents.style.color="#fff";
+            footerBtn.style.color ="#ca2525";
             moonIcon.style.fill = '#5469c7';
         }
     });
